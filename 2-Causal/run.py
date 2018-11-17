cd876ff2231067e0bdb341cfcecf30d89616dbdf
GDPlumb <plumbdg@gmail.com>
2018-11-16 21:17:39 -0500

    Updated UCI run files

--- 2-Causal/run.py
+++ 2-Causal/run.py
@@ -18,6 +18,7 @@ datasets = ["autompgs", "communities", "day", "happiness", "housing", "music", "
 depths = [1,2,3,4]
 sizes = [100, 150, 200, 250, 300]
 rates = [0.0001, 0.001]
+regs = [2.5e3, 5e3, 7.5e3, 1e4, 2.5e4, 5e4, 7.5e4]
 
 # Run function
 def run(args):
@@ -26,8 +27,9 @@ def run(args):
     depth = args[2]
     size = args[3]
     rate = args[4]
+    reg = args[5]
 
-    name = args2name(dataset, trial, depth, size, rate)
+    name = args2name(dataset, trial, depth, size, rate, reg)
 
     cwd = os.getcwd()
 
@@ -37,7 +39,7 @@ def run(args):
     manager = "regression"
     source =  DATASET_PATH + dataset + ".csv"
     shape = [size] * depth
-    out = eval(manager, source, shape, rate)
+    out = eval(manager, source, shape, rate, regularizer = "Causal", c = reg)
 
     with open("out.json", "w") as f:
         json.dump(out, f)
@@ -48,4 +50,4 @@ run_search(run_fn = run, num_processes = 5,
             run_search = True, process_search = True, run_final = True, process_final = True,
             n_search = 2, n_final = 10,
             datasets = datasets, depths = depths, sizes = sizes, rates = rates,
-            regularized = False, regs = None)
+            regularized = True, regs = regs)
