8c343a812dbf7974067324d44ebcf851fd492306
GDPlumb <plumbdg@gmail.com>
2019-05-17 08:25:08 -0400

    Added run scripts

---
+++ run_med.sh
@@ -0,0 +1,15 @@
+
+cd Med-None
+./run.sh
+cd ..
+
+cd Med-LF
+./run.sh
+cd ..
+
+cd Med-LF1D
+./run.sh
+cd ..
+
+cd Med-compare
+Rscript run.R
---
+++ run_uci.sh
@@ -0,0 +1,15 @@
+
+cd UCI-None
+./run.sh
+cd ..
+
+cd UCI-LF
+./run.sh
+cd ..
+
+cd UCI-LF1D
+./run.sh
+cd ..
+
+cd UCI-compare
+Rscript run.R
