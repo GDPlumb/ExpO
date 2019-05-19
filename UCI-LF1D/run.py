23a387edd2a76a96312412089e04a6e113f6a85c
GDPlumb <plumbdg@gmail.com>
2019-05-18 23:41:17 -0400

    Ran evaluation of chosen models

--- UCI-LF1D/run.py
+++ UCI-LF1D/run.py
@@ -56,9 +56,10 @@ def run_fn_search(*args):
     return run_fn(*args, evaluate_explanation = False)
 
 run_search(run_fn_search = run_fn_search, n_search = 10, lower_is_better = True,
-            run_search = True, process_search = True,
+            run_search = False, process_search = False,
             run_fn_final = run_fn, n_final = 20,
-            run_final = False, process_final = False,
+            run_final = True, process_final = True,
             datasets = datasets, source = "../UCI-None/config.json", #depths = depths, sizes = sizes, rates = rates,
             regularized = True, regs = regs,
             num_processes = 12)
+
